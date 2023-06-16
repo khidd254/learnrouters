27976558ae359e6ec0254a68c87d6348ae1486ae
khidd <kennedynyambura5981@gmail.com>
2023-06-16 09:22:57 +0300

    init

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Available Jobs</h1>
+ <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+ </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                {title: 'American king', id: '1', details: 'Loream'},
+                {title: 'James Bond', id: '2', details: 'Loream'},
+                {title: 'Mechanic Resurrection', id: '3', details: 'Loream'},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
